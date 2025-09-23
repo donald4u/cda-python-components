bf679860d9e822e47c58cd2806bae16a6b97de2b
donald4u <donaldirebo@gmail.com>
2025-09-23 01:47:53 -0700

    Lab Module 02: Add DeviceDataManager, update ConstrainedDeviceApp, and config changes

---
+++ programmingtheiot/cda/system/DeviceDataManager.py
@@ -0,0 +1,15 @@
+"""
+DeviceDataManager – minimal placeholder
+"""
+
+import logging
+
+class DeviceDataManager:
+    def __init__(self):
+        logging.info("DeviceDataManager initialized")
+
+    def startManager(self):
+        logging.info("DeviceDataManager started")
+
+    def stopManager(self):
+        logging.info("DeviceDataManager stopped")
